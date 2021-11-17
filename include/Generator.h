aed500b20c051076ac769ca6a1666a10e3b84fe6
verybadcoder01 <76253661+verybadcoder01@users.noreply.github.com>
2021-11-17 19:57:51 +0300

    Update Generator.h

--- include/Generator.h
+++ include/Generator.h
@@ -6,4 +6,6 @@
 #define DEEPM_GENERATOR_H
 #include <string>
 std::string generateRealWord(std::string input);
+std::string findMistake(std::string word);
+std::string getRussianWord(const std::string& word);
 #endif //DEEPM_GENERATOR_H
